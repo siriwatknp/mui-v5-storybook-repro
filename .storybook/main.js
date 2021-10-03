f4256963220d7bb87adfff474a8a1f2d00fbda16
siriwatknp <siriwatkunaporn@gmail.com>
2021-10-03 10:05:23 +0700

    fix: config storybook to work with mui v5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const toPath = (filePath) => path.join(process.cwd(), filePath);
+
 module.exports = {
   stories: [
     "../stories/**/*.stories.mdx",
@@ -8,4 +11,17 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-docs",
   ],
+  webpackFinal: async (config) => {
+    return {
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          "@emotion/core": toPath("node_modules/@emotion/react"),
+          "emotion-theming": toPath("node_modules/@emotion/react"),
+        },
+      },
+    };
+  },
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,23 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+import { ThemeProvider as EmotionThemeProvider } from "emotion-theming";
+
+const defaultTheme = createTheme();
+
+const withThemeProvider = (Story, context) => {
+  return (
+    <EmotionThemeProvider theme={defaultTheme}>
+      <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
+        <Story {...context} />
+      </ThemeProvider>
+    </EmotionThemeProvider>
+  );
+};
+
+export const decorators = [withThemeProvider];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
